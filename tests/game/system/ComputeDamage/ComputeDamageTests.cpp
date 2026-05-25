5c9a3154877e89a2e0eb0ec57e7f561ed2330858
comeChaslerie <come.chaslerie@epitech.eu>
2026-05-25 14:26:01 +0200

    feat(ComputeDamageTest.cpp): falloff tests added

--- tests/game/system/ComputeDamage/ComputeDamageTests.cpp
+++ tests/game/system/ComputeDamage/ComputeDamageTests.cpp
@@ -11,3 +11,11 @@ TEST(ComputeDamage, TypeModifiers)
     EXPECT_FLOAT_EQ(ComputeDamage(100.f, DamageType::Explosion), 150.f);
     EXPECT_FLOAT_EQ(ComputeDamage(100.f, DamageType::Environment), 100.f);
 }
+
+TEST(ComputeDamage, Falloff)
+{
+    EXPECT_FLOAT_EQ(ComputeDamage(100.f, DamageType::Hitscan, 0), 100.f);
+    EXPECT_FLOAT_EQ(ComputeDamage(100.f, DamageType::Projectile, 0.5), 50.f);
+    EXPECT_FLOAT_EQ(ComputeDamage(100.f, DamageType::Explosion, 2), 0.f);
+    EXPECT_FLOAT_EQ(ComputeDamage(100.f, DamageType::Environment, 0.25), 75.f);
+}
