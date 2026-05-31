0b32519979e236f425a2c25c58ccc6298e26d4ba
comeChaslerie <come.chaslerie@epitech.eu>
2026-05-31 22:22:03 +0200

    refactor(main.cpp): plugin CombatPlugin replace ApplyDamage system

--- src/main.cpp
+++ src/main.cpp
@@ -5,9 +5,9 @@
 #include "game/component/Damage/DamageEvent.hpp"
 #include "game/component/Damage/DamageType.hpp"
 #include "game/component/Health.hpp"
-#include "game/system/ApplyDamage/ApplyDamage.hpp"
 #include "scheduler/Startup.hpp"
 #include "scheduler/Update.hpp"
+#include "game/plugin/Combat/CombatPlugin.hpp"
 
 using namespace game::component;
 
@@ -16,12 +16,12 @@ int main()
     Engine::Core core;
 
     core.RegisterSystem<Engine::Scheduler::Startup>([](Engine::Core &core) {
-        Engine::Entity entity = core.CreateEntity();
+        auto entity = core.CreateEntity();
         Health &health = entity.AddComponent<Health>();
         entity.AddComponent<DamageEvent>(25.f, DamageType::Hitscan);
         Log::Info(fmt::format("Spawn : {}/{} PV", health.hp.Current(), health.hp.Max()));
     });
-    core.RegisterSystem<Engine::Scheduler::Update>(game::system::ApplyDamage);
+    core.AddPlugins<game::plugin::CombatPlugin>();
     core.RegisterSystem<Engine::Scheduler::Update>([](Engine::Core &core) {
         core.GetRegistry().view<Health>().each(
             [](Health &health) { Log::Info(fmt::format("After : {}/{} PV", health.hp.Current(), health.hp.Max())); });
