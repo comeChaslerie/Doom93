6464004dd74704e9e55742279e775430eead78a0
comeChaslerie <come.chaslerie@epitech.eu>
2026-06-04 13:06:50 +0200

    feat(FileReader.cpp, FileReader.hpp): utils FileReader functions and constexpr added

---
+++ src/utils/FileReader/FileReader.cpp
@@ -0,0 +1,25 @@
+#include <cstddef>
+#include <fstream>
+#include "utils/FileReader/FileReader.hpp"
+
+void utils::FileReader::checkFileIntegrity(std::ifstream &file, size_t nbBytes, bool checkEnd)
+{
+    if ((checkEnd && !file) || (nbBytes && file.gcount() != nbBytes))
+        throw utils::FileReader::FileReaderError("Truncated file");
+}
+
+std::string utils::FileReader::GetString(std::ifstream &file, size_t size)
+{
+    std::vector<char> buf(size);
+
+    file.read(buf.data(), static_cast<std::streamsize>(size));
+    return std::string(buf.data(), size);
+}
+
+int32_t utils::FileReader::GetInt32(std::ifstream &file)
+{
+    int32_t int_value = 0;
+
+    file.read(reinterpret_cast<char *>(&int_value), utils::FileReader::int32Size);
+    return int_value;
+}
---
+++ src/utils/FileReader/FileReader.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <stdexcept>
+
+namespace utils::FileReader {
+class FileReaderError : public std::runtime_error {
+    using std::runtime_error::runtime_error;
+};
+void checkFileIntegrity(std::ifstream &file, size_t nbBytes, bool checkEnd);
+[[ nodiscard ]] int32_t GetInt32(std::ifstream &file);
+[[ nodiscard ]] std::string GetString(std::ifstream &file, size_t size);
+constexpr size_t int32Size = 4;
+}
