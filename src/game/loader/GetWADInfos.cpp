112dc77122a8c2b77d3450af3dbd13f35a7b16fe
comeChaslerie <come.chaslerie@epitech.eu>
2026-06-05 14:41:01 +0200

    refactor(GetWADInfos.cpp, GetWADInfos.hpp): refactor with enum class CheckState and formatted

--- src/game/loader/GetWADInfos.cpp
+++ src/game/loader/GetWADInfos.cpp
@@ -1,18 +1,18 @@
 #include "game/loader/GetWADInfos.hpp"
+#include "utils/FileReader/FileReader.hpp"
 #include <cstddef>
 #include <cstdlib>
-#include "utils/FileReader/FileReader.hpp"
 
 void utils::FileReader::GetHeader(FileReader &file, game::loader::WadInfo &wadInfos)
 {
     wadInfos.magic = file.GetString(magicSize);
-    file.CheckFileIntegrity(magicSize, true);
+    file.CheckFileIntegrity(magicSize, FileReader::CheckState::BytesAndStream);
     if (wadInfos.magic != "IWAD" && wadInfos.magic != "PWAD")
         throw game::loader::WadLoadError("bad WAD magic " + wadInfos.magic);
     wadInfos.nb_lumps = file.GetInt32();
-    file.CheckFileIntegrity(file._int32Size, true);
+    file.CheckFileIntegrity(file._int32Size, FileReader::CheckState::BytesAndStream);
     wadInfos.offset = file.GetInt32();
-    file.CheckFileIntegrity(file._int32Size, true);
+    file.CheckFileIntegrity(file._int32Size, FileReader::CheckState::BytesAndStream);
 }
 
 void utils::FileReader::GetWADDirectory(FileReader &file, game::loader::WadInfo &wadInfos)
@@ -22,17 +22,17 @@ void utils::FileReader::GetWADDirectory(FileReader &file, game::loader::WadInfo 
     if (wadInfos.offset < 0 || wadInfos.nb_lumps < headerEnd)
         throw game::loader::WadLoadError("bad header infos");
     file.Seek(wadInfos.offset);
-    file.CheckFileIntegrity(0, true);
+    file.CheckFileIntegrity(0, FileReader::CheckState::StreamOnly);
     wadInfos.lumps.reserve(wadInfos.nb_lumps);
     for (int32_t index = 0; index < wadInfos.nb_lumps; index++)
     {
-        file.CheckFileIntegrity(0, true);
+        file.CheckFileIntegrity(0, FileReader::CheckState::StreamOnly);
         lump.filePos = file.GetInt32();
-        file.CheckFileIntegrity(file._int32Size, true);
+        file.CheckFileIntegrity(file._int32Size, FileReader::CheckState::BytesAndStream);
         lump.size = file.GetInt32();
-        file.CheckFileIntegrity(file._int32Size, true);
+        file.CheckFileIntegrity(file._int32Size, FileReader::CheckState::BytesAndStream);
         lump.name = file.GetString(nameSize);
-        file.CheckFileIntegrity(nameSize, false);
+        file.CheckFileIntegrity(nameSize, FileReader::CheckState::BytesOnly);
         wadInfos.lumps.push_back(lump);
     }
 }
--- src/game/loader/GetWADInfos.hpp
+++ src/game/loader/GetWADInfos.hpp
@@ -1,7 +1,7 @@
 #pragma once
 
-#include "utils/FileReader/FileReader.hpp"
 #include "game/loader/WadLoader.hpp"
+#include "utils/FileReader/FileReader.hpp"
 
 namespace utils::FileReader {
 void GetWADDirectory(FileReader &file, game::loader::WadInfo &wadInfos);
@@ -10,4 +10,4 @@ void GetWADInfos(FileReader &file, game::loader::WadInfo &wadInfos);
 constexpr size_t magicSize = 4;
 constexpr size_t nameSize = 8;
 constexpr int32_t headerEnd = 12;
-}
+} // namespace utils::FileReader
