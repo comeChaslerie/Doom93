0b8c48b87dc6f7edbacbb6e7a2152557bee7b180
comeChaslerie <come.chaslerie@epitech.eu>
2026-06-06 16:26:24 +0200

    style(main.cpp): main file clang-formatted

--- src/main.cpp
+++ src/main.cpp
@@ -6,13 +6,13 @@
 #include "game/component/Damage/DamageType.hpp"
 #include "game/component/Death/DeathEvent.hpp"
 #include "game/component/Health.hpp"
+#include "game/loader/WadLoader.hpp"
+#include "game/plugin/Combat/CombatPlugin.hpp"
 #include "game/system/DeathSystem/DeathSystem.hpp"
 #include "plugin/PluginEvent.hpp"
 #include "resource/EventManager.hpp"
 #include "scheduler/Startup.hpp"
 #include "scheduler/Update.hpp"
-#include "game/plugin/Combat/CombatPlugin.hpp"
-#include "game/loader/WadLoader.hpp"
 
 #include <exception>
 
@@ -52,12 +52,9 @@ int main()
     core.AddPlugins<game::plugin::CombatPlugin>();
     core.AddPlugins<Event::Plugin>();
     auto &events = core.GetResource<Event::Resource::EventManager>();
-    events.RegisterCallback<DeathEvent>([](const DeathEvent &e) {
-        Log::Info(fmt::format("DeathEvent: entity {} is dead", e.entity));
-    });
-    events.RegisterCallback<DeathEvent>([&deaths](const DeathEvent &e) {
-        ++deaths;
-    });
+    events.RegisterCallback<DeathEvent>(
+        [](const DeathEvent &e) { Log::Info(fmt::format("DeathEvent: entity {} is dead", e.entity)); });
+    events.RegisterCallback<DeathEvent>([&deaths](const DeathEvent &e) { ++deaths; });
     core.RegisterSystem<Engine::Scheduler::Update>([](Engine::Core &core) {
         core.GetRegistry().view<Health>().each(
             [](Health &health) { Log::Info(fmt::format("After : {}/{} PV", health.hp.Current(), health.hp.Max())); });
