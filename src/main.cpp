ffc48eb2c367499aff76afa34226dc0ff04aa494
comeChaslerie <come.chaslerie@epitech.eu>
2026-06-08 14:32:14 +0200

    refactor(main.cpp): remove ancient systems for HealthPlugin working proof

--- src/main.cpp
+++ src/main.cpp
@@ -7,9 +7,7 @@
 #include "game/component/Death/DeathEvent.hpp"
 #include "game/component/Health.hpp"
 #include "game/loader/WadLoader.hpp"
-#include "game/plugin/Combat/CombatPlugin.hpp"
-#include "game/system/DeathSystem/DeathSystem.hpp"
-#include "plugin/PluginEvent.hpp"
+#include "game/plugin/Health/HealthPlugin.hpp"
 #include "resource/EventManager.hpp"
 #include "scheduler/Startup.hpp"
 #include "scheduler/Update.hpp"
@@ -24,10 +22,6 @@ void LoadWad(const std::string &path)
     try
     {
         const game::loader::LumpData wad = game::loader::WadLoader(path);
-        Log::Info(fmt::format("WAD '{}' charge : {} niveaux, {} palettes, {} colormaps, {} pnames, {} textures, {} "
-                              "flats, {} sprites/patches",
-                              path, wad.levels.size(), wad.palettes.size(), wad.colormaps.size(), wad.pnames.size(),
-                              wad.textures.size(), wad.flats.size(), wad.pictures.size()));
     }
     catch (const std::exception &error)
     {
@@ -45,22 +39,14 @@ int main()
 
     core.RegisterSystem<Engine::Scheduler::Startup>([](Engine::Core &core) {
         auto entity = core.CreateEntity();
-        auto &health = entity.AddComponent<Health>();
+        entity.AddComponent<Health>();
         entity.AddComponent<DamageEvent>(150.f, DamageType::Hitscan);
-        Log::Info(fmt::format("Spawn : {}/{} PV", health.hp.Current(), health.hp.Max()));
     });
-    core.AddPlugins<game::plugin::CombatPlugin>();
-    core.AddPlugins<Event::Plugin>();
-    auto &events = core.GetResource<Event::Resource::EventManager>();
-    events.RegisterCallback<DeathEvent>(
-        [](const DeathEvent &e) { Log::Info(fmt::format("DeathEvent: entity {} is dead", e.entity)); });
-    events.RegisterCallback<DeathEvent>([&deaths](const DeathEvent &e) { ++deaths; });
+    core.AddPlugins<game::plugin::HealthPlugin>();
+    auto &events = core.GetResource<Event::Resource::EventManager>();events.RegisterCallback<DeathEvent>([&deaths](const DeathEvent &e) { ++deaths; });
     core.RegisterSystem<Engine::Scheduler::Update>([](Engine::Core &core) {
-        core.GetRegistry().view<Health>().each(
-            [](Health &health) { Log::Info(fmt::format("After : {}/{} PV", health.hp.Current(), health.hp.Max())); });
         core.Stop();
     });
-    core.RegisterSystem<Engine::Scheduler::Update>(game::system::DeathSystem);
     core.Run();
     Log::Info(fmt::format("Total deaths counted: {}", deaths));
     return 0;
